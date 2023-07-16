a5c8906d63ca74c046c1f390df2fa1a8c91b6863
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-07-16 19:00:07 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,29 @@
 const featuresButton = document.getElementById("features-btn");
-// const featuresDropdown = document.getElementById("dropdown-features");
+// const featuresDropdown = document.getElementById("dropdown-features")
 const featuresContent = document.getElementById("dropdown-features-content");
+const featuresArrowOpen = document.getElementById("features-arrow-open");
+const featuresArrowClose = document.getElementById("features-arrow-close");
+const companyButton = document.getElementById("company-btn");
+const companyContent = document.getElementById("company-content");
+const companyArrowOpen = document.getElementById("company-arrow-open");
+const companyArrowClose = document.getElementById("company-arrow-close");
 
 featuresButton.addEventListener("click", () => {
   featuresContent.classList.toggle("hidden");
+  featuresArrowOpen.classList.toggle("hidden");
+  featuresArrowClose.classList.toggle("hidden");
+});
+
+companyButton.addEventListener("click", () => {
+  companyContent.classList.toggle("hidden");
+  companyArrowOpen.classList.toggle("hidden");
+  companyArrowClose.classList.toggle("hidden");
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === featuresButton.parentElement) {
+    // if (e.target !== featuresContent) {
+    console.log("22");
+    // }
+  }
 });
